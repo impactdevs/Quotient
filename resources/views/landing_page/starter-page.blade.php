4ee5d4bcb2c5f3dd69e73529ab2abecf5e41ee6b
Dave <ochwodavid03@gmail.com>
2025-01-10 05:32:54 -0800

    newsletter removed

--- resources/views/landing_page/starter-page.blade.php
+++ resources/views/landing_page/starter-page.blade.php
@@ -60,25 +60,9 @@
       @yield('content') 
   </main>
 
-  <footer id="footer" class="footer position-relative">
+  <footer id="footer" class="footer position-relative mt-5">
 
-    <div class="footer-newsletter">
-      <div class="container">
-        <div class="row justify-content-center text-center">
-          <div class="col-lg-6">
-            <h4>Join Our Newsletter</h4>
-            <p>Subscribe to our newsletter and receive the latest news about Quotient software and other related technologies!</p>
-            <form action="forms/newsletter.php" method="post" class="php-email-form">
-              <div class="newsletter-form"><input type="email" name="email"><input type="submit" value="Subscribe"></div>
-              <div class="loading">Loading</div>
-              <div class="error-message"></div>
-              <div class="sent-message">Your subscription request has been sent. Thank you!</div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-
+   
     <div class="container footer-top">
       <div class="row gy-4">
         <div class="col-lg-4 col-md-6 footer-about">
