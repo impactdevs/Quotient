8d3f799ccd5d62f1862a0befa3ed93c310d46bfe
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-02-28 12:38:35 +0300

    new update

--- resources/views/landing_page/landing_page.blade.php
+++ resources/views/landing_page/landing_page.blade.php
@@ -650,16 +650,16 @@
     <div id="preloader"></div>
 
     <!-- Vendor JS Files -->
-    <script src="{{ asset(' assets/landing_page/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset(' assets/landing_page/assets/vendor/php-email-form/validate.js') }}"></script>
-    <script src="{{ asset(' assets/landing_page/assets/vendor/aos/aos.js') }}"></script>
-    <script src="{{ asset(' assets/landing_page/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-    <script src="{{ asset(' assets/landing_page/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
-    <script src="{{ asset(' assets/landing_page/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-    <script src="{{ asset(' assets/landing_page/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/landing_page/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/landing_page/assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('assets/landing_page/assets/vendor/aos/aos.js') }}"></script>
+    <script src="{{ asset('assets/landing_page/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('assets/landing_page/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/landing_page/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/landing_page/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
 
     <!-- Main JS File -->
-    <script src="{{ asset('resources\js\main.js') }}"></script>
+    <script src="resources/js/main.js"></script>
 
     <script>
         /**
